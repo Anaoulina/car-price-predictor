0a7fd15dce28e26e5ad04cabe5fa9ff331f4505f
Anaoulina <l.anaou5892@uca.ac.ma>
2026-04-15 20:29:22 +0100

    Step 4: Frontend code

--- backend_api/app/main.py
+++ backend_api/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from backend_api.app.schemas import CarInput
 from backend_api.services.predictor import predict_price
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Car Price Prediction API")
 
@@ -12,3 +13,12 @@ def home():
 def predict(data: CarInput):
     price = predict_price(data)
     return {"predicted_price": price}
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
